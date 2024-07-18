e38ea4e7189d8797cf452cd17dc3ef37a5cce74d
Tanmay-codeol <tanmaysharma0852@gmail.com>
2024-07-18 09:37:03 +0530

    only req address

--- Solana-token-swap.js
+++ Solana-token-swap.js
@@ -1,5 +1,5 @@
 const solanaWeb3 = require('@solana/web3.js');
-const { Market, OpenOrders } = require('@project-serum/serum');
+const { Token, TOKEN_PROGRAM_ID } = require('@solana/spl-token');
 
 const SOL_MINT_ADDRESS = "So11111111111111111111111111111111111111112";
 const DOGWIFTHAT_MINT_ADDRESS = "EKpQGSJtjMFqKZ9KQanSqYXRcF8fBopzLHYxdM65zcjm";
@@ -9,38 +9,75 @@ const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-b
 async function swapTokens() {
     const payer = solanaWeb3.Keypair.fromSecretKey(new Uint8Array(JSON.parse(process.env.SOLANA_SECRET_KEY)));
 
-    const marketAddress = new solanaWeb3.PublicKey('Market_Address'); 
-    const market = await Market.load(connection, marketAddress, {}, solanaWeb3.PublicKey.default);
+    const solMint = new solanaWeb3.PublicKey(SOL_MINT_ADDRESS);
+    const dogwifthatMint = new solanaWeb3.PublicKey(DOGWIFTHAT_MINT_ADDRESS);
 
-    const baseMint = new solanaWeb3.PublicKey(SOL_MINT_ADDRESS);
-    const quoteMint = new solanaWeb3.PublicKey(DOGWIFTHAT_MINT_ADDRESS);
+    const solToken = new Token(connection, solMint, TOKEN_PROGRAM_ID, payer);
+    const dogwifthatToken = new Token(connection, dogwifthatMint, TOKEN_PROGRAM_ID, payer);
 
-    const owner = payer.publicKey;
-    const openOrders = await OpenOrders.findForOwner(connection, owner, market.address, solanaWeb3.PublicKey.default);
-    
-    const [baseVault, quoteVault] = await Promise.all([
-        market.findOpenOrdersAccountsForOwner(owner),
-        market.findOpenOrdersAccountsForOwner(owner),
-    ]);
+    const solTokenAccount = await solToken.getOrCreateAssociatedAccountInfo(payer.publicKey);
+    const dogwifthatTokenAccount = await dogwifthatToken.getOrCreateAssociatedAccountInfo(payer.publicKey);
+const solanaWeb3 = require('@solana/web3.js');
+const { Token, TOKEN_PROGRAM_ID } = require('@solana/spl-token');
 
-    const transaction = new solanaWeb3.Transaction();
+const SOL_MINT_ADDRESS = "So11111111111111111111111111111111111111112";
+const DOGWIFTHAT_MINT_ADDRESS = "EKpQGSJtjMFqKZ9KQanSqYXRcF8fBopzLHYxdM65zcjm";
 
-    const instruction = market.makePlaceOrderInstruction(connection, {
-        owner,
-        payer: baseVault[0].baseTokenAccount,
-        side: 'sell', 
-        price: 1.0, 
-        size: 1.0, 
-        orderType: 'limit',
-        clientId: new solanaWeb3.BN(123),
-        openOrdersAddressKey: openOrders[0].address,
-        feeDiscountPubkey: null,
-    });
+const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'), 'confirmed');
 
-    transaction.add(instruction);
+async function swapTokens() {
+    const payer = solanaWeb3.Keypair.fromSecretKey(new Uint8Array(JSON.parse(process.env.SOLANA_SECRET_KEY)));
+
+    const solMint = new solanaWeb3.PublicKey(SOL_MINT_ADDRESS);
+    const dogwifthatMint = new solanaWeb3.PublicKey(DOGWIFTHAT_MINT_ADDRESS);
+
+    const solToken = new Token(connection, solMint, TOKEN_PROGRAM_ID, payer);
+    const dogwifthatToken = new Token(connection, dogwifthatMint, TOKEN_PROGRAM_ID, payer);
+
+    const solTokenAccount = await solToken.getOrCreateAssociatedAccountInfo(payer.publicKey);
+    const dogwifthatTokenAccount = await dogwifthatToken.getOrCreateAssociatedAccountInfo(payer.publicKey);
+
+    const amountToSwap = 1 * solanaWeb3.LAMPORTS_PER_SOL;
+
+    const transaction = new solanaWeb3.Transaction().add(
+        solToken.createTransferInstruction(
+            solTokenAccount.address,
+            dogwifthatTokenAccount.address,
+            payer.publicKey,
+            amountToSwap,
+            []
+        )
+    );
+
     transaction.feePayer = payer.publicKey;
-    const recentBlockhash = await connection.getRecentBlockhash();
-    transaction.recentBlockhash = recentBlockhash.blockhash;
+    const { blockhash } = await connection.getRecentBlockhash();
+    transaction.recentBlockhash = blockhash;
+
+    transaction.sign(payer);
+
+    const signature = await connection.sendRawTransaction(transaction.serialize());
+    await connection.confirmTransaction(signature);
+
+    console.log("Transaction completed with signature:", signature);
+}
+
+swapTokens().catch(console.error);
+
+    const amountToSwap = 1 * solanaWeb3.LAMPORTS_PER_SOL;
+
+    const transaction = new solanaWeb3.Transaction().add(
+        solToken.createTransferInstruction(
+            solTokenAccount.address,
+            dogwifthatTokenAccount.address,
+            payer.publicKey,
+            amountToSwap,
+            []
+        )
+    );
+
+    transaction.feePayer = payer.publicKey;
+    const { blockhash } = await connection.getRecentBlockhash();
+    transaction.recentBlockhash = blockhash;
 
     transaction.sign(payer);
 
